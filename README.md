3ea5e45a8c4a11254f9d774dd3f6ae848a4da397
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-07-26 13:23:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # seneca-counter-firebase
 Counter microservice with a firebase backend
+
+Work in progress
+
+## Usage
+
+Start service
+
+```javascript
+'use strict'
+
+const Seneca = require('seneca')()
+const senecaCounter = require('seneca-counter-firebase')
+const senecaCounterOptions = {
+  tag: 'seneca-counter-firebase',
+  apiKey: '<your-firebase-api-key>',
+  authDomain: '<your-firebase-auth-domain>',
+  databaseURL: '<your-firebase-database-url>',
+  storageBucket: '<your-firebase-storage-bucket>'
+}
+
+Seneca.use(senecaCounter, senecaCounterOptions)
+
+Seneca.listen(1337)
+
+```
+
+call the service to add a value for a given key
+
+```sh
+$ curl -d '{"role":"counter", "cmd":"add", "key": "tfk", "value": 2}' -v http://localhost:1337/act
+```
+
+or to subtract
+
+```sh
+$ curl -d '{"role":"counter", "cmd":"subtract", "key": "tfk", "value": 2}' -v http://localhost:1337/act
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = function firebaseCounter (options) {
+  const firebase = require('firebase')
+  const app = firebase.initializeApp({
+    apiKey: options.apiKey,
+    authDomain: options.authDomain,
+    databaseURL: options.databaseURL,
+    storageBucket: options.storageBucket
+  })
+  const database = firebase.database()
+  const seneca = this
+
+  seneca.add('role:counter,cmd:add', (args, done) => {
+    const value = parseInt(args.value || 1, 10)
+    const valueRef = database.ref(args.key || 'value')
+    valueRef.transaction((currentVal) => {
+      return currentVal + value
+    })
+    done(null, {success: true, value: value})
+  })
+
+  seneca.add('role:counter,cmd:subtract', (args, done) => {
+    const value = parseInt(args.value || 1, 10)
+    const valueRef = database.ref(args.key || 'value')
+    valueRef.transaction((currentVal) => {
+      return currentVal - value
+    })
+    done(null, {success: true, value: value})
+  })
+
+  return options.tag || 'seneca-counter-firebase'
+}
